9fafcbfbc9cc29a1430db79cca2c3f7068a6341c
ngala001 <ngala.junior07@gmail.com>
2025-07-21 19:07:55 +0000

    remove unecessary pages

--- src/app/(Tasks)/page.jsx
+++ src/app/(Tasks)/page.jsx
@@ -1,24 +1,11 @@
-import ProductCard from "@/components/ProductCard";
-/* import TaskCard from "@/components/TaskCard"; */
-
-const fetchProducts = async() => {
-  const res = await fetch('https://fakestoreapi.com/products')
-  const data = await res.json()
-  return data
-}
+import TaskCard from "@/components/TaskCard";
 
 export default async function Home() {
-  const products = await fetchProducts()
+
   return (
-   <div className="container grid gap-10 grid-cols-3 mx-auto mt-7">
-     {
-        products.map(product => {
-           return (
-             <ProductCard key={product.id} product={product} />
-           )
-        })
-    }
-   </div>
+    <div className="mt-7">
+      <TaskCard />
+    </div>
   );
 }
 
